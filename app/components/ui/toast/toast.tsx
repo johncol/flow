5f54ed0ddfbd7c5aa18b450f1b657f3b3c90ce4b
John Cely <john.19col@gmail.com>
2026-01-03 20:16:56 -0800

    Set aria labels in toast component and error trigger

--- app/components/ui/toast/toast.tsx
+++ app/components/ui/toast/toast.tsx
@@ -5,13 +5,17 @@ export const Toaster = () => {
     <HotToaster
       position="bottom-right"
       toastOptions={{
-        duration: 4000,
+        duration: 6000,
         style: {
           borderRadius: "1rem",
           boxShadow: "none",
           border: "1px solid #e0e0e0",
           padding: "1rem 2rem",
           fontSize: "1.4rem",
+        },
+        ariaProps: {
+          role: "status",
+          "aria-live": "polite",
         },
       }}
     />
@@ -29,6 +33,7 @@ export const showSuccessToast = (message: string) => {
  * Show an error toast notification
  */
 export const showErrorToast = (message: string) => {
-  toast.error(message);
+  toast.error(message, {
+    ariaProps: { role: "alert", "aria-live": "assertive" },
+  });
 };
-
