4831794de0ef48034d1603a515dd63314632ea3a
John Cely <john.19col@gmail.com>
2026-01-03 21:42:30 -0800

    Add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), vanillaExtractPlugin()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    vanillaExtractPlugin(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"], // if you use TypeScript
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
 });
